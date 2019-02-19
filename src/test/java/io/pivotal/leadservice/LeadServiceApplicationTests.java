903e2a2b7b1bfa6b735297d500d2f124f9171c59
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-02-19 13:20:19 -0500

    Initial start.spring.io skeleton

---
+++ src/test/java/io/pivotal/leadservice/LeadServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.leadservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeadServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
